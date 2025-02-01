3e02a10be60b796408906aebdd8cb956db868552
tyro_xc <3176926250@qq.com>
2025-02-02 01:42:48 +0800

    function done

--- src/views/SearchGithubName.vue
+++ src/views/SearchGithubName.vue
@@ -1,37 +1,36 @@
 <template>
   <h1>SearchGithubName</h1>
-  <span>https:api.github.com/search/users?q=</span><br />
-  <!-- <Search /> -->
+  <span>https://api.github.com/search/users?q=</span><br />
   <input type="text" v-model="input" />
   <button @click="search">search</button>
   <textarea v-model="textarea" cols="30" rows="10"></textarea>
-  <ul>
-    <li v-for="i in arr">
-      <button>{{ i.login }}</button>
-      <button>{{ i.id }}</button>
-    </li>
-  </ul>
+  <div class="list" style="display: flex; flex-wrap: wrap">
+    <div
+      class="card"
+      style="width: 100px; height: 100px; border: 1px solid #ccc; margin: 5px"
+      v-for="item in arr"
+      :key="item.id"
+    >
+      <a :href="item.html_url">{{ item.login }}</a>
+      <img :src="item.avatar_url" alt="" width="100%" />
+      <p>{{ item.id }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
-// import Search from '../components/SearchGithubName/Search.vue';
 import { ref, onMounted, reactive } from "vue";
-// let URL = "http:api.github.com/searcu/users?q=";
 const input = ref("");
 const textarea = ref("");
-const arr = reactive([]);
+const arr = ref([]); // 使用 ref 而不是 reactive
 
-const search = function () {
-  // console.log('input',input.value);
-  fetch("https:api.github.com/search/users?q=" + input.value)
-    .then((response) => response.text())
+const search = async function () {
+  await fetch("https://api.github.com/search/users?q=" + input.value)
+    .then((response) => response.json())
     .then((data) => {
-      textarea.value = data;
+      textarea.value = JSON.stringify(data, null, 2);
       console.table(data);
-      arr = data.items;
+      arr.value = data.items; // 使用 .value 来更新响应式数组
     });
-  alert("123");
 };
-
-// onMounted()
 </script>
