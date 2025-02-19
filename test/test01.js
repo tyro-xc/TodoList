1a4d1e82d07f496c3fead6d60c296043e5f0e26a
tyro_xc <3176926250@qq.com>
2025-02-20 04:43:56 +0800

    123

---
+++ test/test01.js
@@ -0,0 +1,36 @@
+// const str = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+//     A natus est labore reiciendis nemo illum doloremque aperiam? Molestiae
+//      vel accusamus, debitis doloribus sed asperiores, ad, magnam minus 
+//      neque delectus odit?`;
+// const result = [];
+
+// for (const c of str) {
+//   console.log(c);
+// }
+
+// let url =
+//   "https://api.openweathermap.org/data/2.5/forecast?q=Beijing,cn&appid=800f49846586c3ba6e7052cfc89af16c";
+// fetch(url).then((resp) => {
+//   resp.json().then((data) => {
+//     let info = {
+//       city: data.city.name,
+//       weather: data.list[0].weather[0].main,
+//       time: data.list[0].dt_txt,
+//     };
+//     alert(JSON.stringify(info, null, "  "));
+//   });
+// });
+
+// //  for (const c in global) {
+// //     console.log("____________________");
+// //     console.log(c,global[c]);
+// //  }
+// // //  console.log(global);
+function abs() {
+    if (arguments.length === 0) {
+        return 0;
+    }
+    let x = arguments[0];
+    return x >= 0 ? x : -x;
+}
+console.log(abs(1,2,3));
