70731e0968341433e263f0db2fe46f4eaa430cb4
tyro-xc <3176926250@qq.com>
2025-03-01 16:55:46 +0000

    123

--- test/test02.js
+++ test/test02.js
@@ -1,16 +1,27 @@
-// var a=10;
-
-// (function a(){
-//   a=20;
-//   console.log(a);
-// })();
-
-// console.log(a);
-
-function A(){
-    A=100;
-    console.log(A);
-    console.log();  
+// const arr=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+const arr=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+//random array element
+function getRandomEle(arr ){
+    return arr[Math.floor(Math.random()*(arr.length) )]
 }
+// per word's random length
+function generateWord(){
+    let word=[]
+    for(let i=0;i<10;i++){
+        word.push(getRandomEle(arr))
+    }
+    return word.join('')
+}
+//random sentence 
 
-console.log(A);
+function generateRandomSentence(){
+    let sentence=[]
+    for(let i=0;i<10;i++){
+        sentence.push(generateWord(arr))
+    }
+    return sentence.join('')
+}
+// console.log(getRandomEle(arr));
+
+console.log(generateWord());
+console.log(generateRandomSentence());
